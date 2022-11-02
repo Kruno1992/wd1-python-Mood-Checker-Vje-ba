56f34ff1872545194c1ec152b77d4df944ba3ea4
KrunoslavTheMoth <krunoswtor@gmail.com>
2022-11-02 12:16:56 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+mood = input("What is your mood today?")
+
+if mood == "happy" :
+    print("It is great to see you happy!")
+
+elif mood == "nervous" :
+    print("Take a deep breath 3 times.")
+
+elif mood == "sad" :
+    print("Take a candy!")
+
+elif mood == "excited" :
+    print("Go and run!")
+
+elif mood == "relaxed" :
+    print("Fun day, isnt it?")
+
+else :
+    print("I dont recognize this mood")
